0bd638f8c0788fb1fa848d94e7fea9b728a4d46e
Vizniuk_Nykyta <nikitos.v.leo@gmail.com>
2024-10-24 23:34:56 +0300

    Добавив кнопки, +- зробив їхній вигляд(Н)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            slotyButton = new PictureBox();
+            ruletkaButton = new PictureBox();
+            kostyButton = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)slotyButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ruletkaButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kostyButton).BeginInit();
             SuspendLayout();
+            // 
+            // slotyButton
+            // 
+            slotyButton.Anchor = AnchorStyles.None;
+            slotyButton.Image = Properties.Resources.sloty;
+            slotyButton.Location = new Point(411, 183);
+            slotyButton.Name = "slotyButton";
+            slotyButton.Size = new Size(300, 113);
+            slotyButton.SizeMode = PictureBoxSizeMode.Zoom;
+            slotyButton.TabIndex = 0;
+            slotyButton.TabStop = false;
+            slotyButton.MouseDown += slotyButton_MouseDown;
+            slotyButton.MouseEnter += slotyButton_MouseEnter;
+            slotyButton.MouseLeave += slotyButton_MouseLeave;
+            slotyButton.MouseUp += slotyButton_MouseUp;
+            // 
+            // ruletkaButton
+            // 
+            ruletkaButton.Anchor = AnchorStyles.None;
+            ruletkaButton.Image = Properties.Resources.ruletka;
+            ruletkaButton.Location = new Point(411, 302);
+            ruletkaButton.Name = "ruletkaButton";
+            ruletkaButton.Size = new Size(300, 113);
+            ruletkaButton.SizeMode = PictureBoxSizeMode.Zoom;
+            ruletkaButton.TabIndex = 1;
+            ruletkaButton.TabStop = false;
+            ruletkaButton.MouseDown += ruletkaButton_MouseDown;
+            ruletkaButton.MouseEnter += ruletkaButton_MouseEnter;
+            ruletkaButton.MouseLeave += ruletkaButton_MouseLeave;
+            ruletkaButton.MouseUp += ruletkaButton_MouseUp;
+            // 
+            // kostyButton
+            // 
+            kostyButton.Anchor = AnchorStyles.None;
+            kostyButton.Image = Properties.Resources.kostya;
+            kostyButton.Location = new Point(411, 421);
+            kostyButton.Name = "kostyButton";
+            kostyButton.Size = new Size(300, 113);
+            kostyButton.SizeMode = PictureBoxSizeMode.Zoom;
+            kostyButton.TabIndex = 2;
+            kostyButton.TabStop = false;
+            kostyButton.MouseDown += kostyButton_MouseDown;
+            kostyButton.MouseEnter += kostyButton_MouseEnter;
+            kostyButton.MouseLeave += kostyButton_MouseLeave;
+            kostyButton.MouseUp += kostyButton_MouseUp;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1148, 565);
+            Controls.Add(kostyButton);
+            Controls.Add(ruletkaButton);
+            Controls.Add(slotyButton);
+            MinimumSize = new Size(300, 500);
             Name = "Form1";
             Text = "Gambling";
+            SizeChanged += Form1_Resize;
+            Resize += Form1_Resize;
+            ((System.ComponentModel.ISupportInitialize)slotyButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ruletkaButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kostyButton).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox slotyButton;
+        private PictureBox ruletkaButton;
+        private PictureBox kostyButton;
     }
 }

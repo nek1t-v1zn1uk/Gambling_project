2668063d69788c537f9d961bfc5704ef5e1c7b88
Vizniuk_Nykyta <nikitos.v.leo@gmail.com>
2024-11-01 18:14:26 +0200

    UserContrlos(Н)

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,97 +29,115 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            kostyButton = new PictureBox();
             axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-            ruletkaButton = new PictureBox();
-            slotyButton = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)kostyButton).BeginInit();
+            gamblingButton = new PictureBox();
+            rakhunokButton = new PictureBox();
+            settingsButton = new PictureBox();
+            exitButton = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)ruletkaButton).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)slotyButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gamblingButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rakhunokButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)settingsButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)exitButton).BeginInit();
             SuspendLayout();
-            // 
-            // kostyButton
-            // 
-            kostyButton.Anchor = AnchorStyles.None;
-            kostyButton.Image = Properties.Resources.kostya;
-            kostyButton.Location = new Point(411, 421);
-            kostyButton.Name = "kostyButton";
-            kostyButton.Size = new Size(300, 113);
-            kostyButton.SizeMode = PictureBoxSizeMode.Zoom;
-            kostyButton.TabIndex = 2;
-            kostyButton.TabStop = false;
-            kostyButton.MouseDown += kostyButton_MouseDown;
-            kostyButton.MouseEnter += kostyButton_MouseEnter;
-            kostyButton.MouseLeave += kostyButton_MouseLeave;
-            kostyButton.MouseUp += kostyButton_MouseUp;
             // 
             // axWindowsMediaPlayer1
             // 
             axWindowsMediaPlayer1.Enabled = true;
-            axWindowsMediaPlayer1.Location = new Point(43, 511);
+            axWindowsMediaPlayer1.Location = new Point(126, 382);
             axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             axWindowsMediaPlayer1.OcxState = (AxHost.State)resources.GetObject("axWindowsMediaPlayer1.OcxState");
             axWindowsMediaPlayer1.Size = new Size(75, 23);
             axWindowsMediaPlayer1.TabIndex = 3;
             // 
-            // ruletkaButton
+            // gamblingButton
             // 
-            ruletkaButton.Anchor = AnchorStyles.None;
-            ruletkaButton.Image = Properties.Resources.ruletka;
-            ruletkaButton.Location = new Point(411, 302);
-            ruletkaButton.Name = "ruletkaButton";
-            ruletkaButton.Size = new Size(300, 113);
-            ruletkaButton.SizeMode = PictureBoxSizeMode.Zoom;
-            ruletkaButton.TabIndex = 1;
-            ruletkaButton.TabStop = false;
-            ruletkaButton.MouseDown += ruletkaButton_MouseDown;
-            ruletkaButton.MouseEnter += ruletkaButton_MouseEnter;
-            ruletkaButton.MouseLeave += ruletkaButton_MouseLeave;
-            ruletkaButton.MouseUp += ruletkaButton_MouseUp;
+            gamblingButton.Anchor = AnchorStyles.None;
+            gamblingButton.BackColor = Color.Transparent;
+            gamblingButton.BackgroundImageLayout = ImageLayout.None;
+            gamblingButton.BorderStyle = BorderStyle.FixedSingle;
+            gamblingButton.Image = Properties.Resources.gambling;
+            gamblingButton.Location = new Point(735, 260);
+            gamblingButton.Name = "gamblingButton";
+            gamblingButton.Size = new Size(450, 125);
+            gamblingButton.SizeMode = PictureBoxSizeMode.Zoom;
+            gamblingButton.TabIndex = 0;
+            gamblingButton.TabStop = false;
             // 
-            // slotyButton
+            // rakhunokButton
             // 
-            slotyButton.Anchor = AnchorStyles.None;
-            slotyButton.Image = Properties.Resources.vihid_hovered;
-            slotyButton.Location = new Point(411, 183);
-            slotyButton.Name = "slotyButton";
-            slotyButton.Size = new Size(300, 113);
-            slotyButton.SizeMode = PictureBoxSizeMode.Zoom;
-            slotyButton.TabIndex = 0;
-            slotyButton.TabStop = false;
-            slotyButton.MouseDown += slotyButton_MouseDown;
-            slotyButton.MouseEnter += slotyButton_MouseEnter;
-            slotyButton.MouseLeave += slotyButton_MouseLeave;
-            slotyButton.MouseUp += slotyButton_MouseUp;
+            rakhunokButton.Anchor = AnchorStyles.None;
+            rakhunokButton.BackColor = Color.Transparent;
+            rakhunokButton.BorderStyle = BorderStyle.FixedSingle;
+            rakhunokButton.Image = Properties.Resources.rahunok;
+            rakhunokButton.Location = new Point(735, 420);
+            rakhunokButton.Name = "rakhunokButton";
+            rakhunokButton.Size = new Size(450, 125);
+            rakhunokButton.SizeMode = PictureBoxSizeMode.Zoom;
+            rakhunokButton.TabIndex = 4;
+            rakhunokButton.TabStop = false;
+            // 
+            // settingsButton
+            // 
+            settingsButton.Anchor = AnchorStyles.None;
+            settingsButton.BackColor = Color.Transparent;
+            settingsButton.BorderStyle = BorderStyle.FixedSingle;
+            settingsButton.Image = Properties.Resources.nalashtuvanya;
+            settingsButton.Location = new Point(735, 580);
+            settingsButton.Name = "settingsButton";
+            settingsButton.Size = new Size(450, 125);
+            settingsButton.SizeMode = PictureBoxSizeMode.Zoom;
+            settingsButton.TabIndex = 5;
+            settingsButton.TabStop = false;
+            // 
+            // exitButton
+            // 
+            exitButton.Anchor = AnchorStyles.None;
+            exitButton.BackColor = Color.Transparent;
+            exitButton.BorderStyle = BorderStyle.FixedSingle;
+            exitButton.Image = Properties.Resources.vihid;
+            exitButton.Location = new Point(735, 740);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(450, 125);
+            exitButton.SizeMode = PictureBoxSizeMode.Zoom;
+            exitButton.TabIndex = 6;
+            exitButton.TabStop = false;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background_2;
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(1148, 565);
             Controls.Add(axWindowsMediaPlayer1);
-            Controls.Add(kostyButton);
-            Controls.Add(ruletkaButton);
-            Controls.Add(slotyButton);
+            Controls.Add(exitButton);
+            Controls.Add(settingsButton);
+            Controls.Add(rakhunokButton);
+            Controls.Add(gamblingButton);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
             MinimumSize = new Size(300, 500);
             Name = "MainForm";
             Text = "Gambling";
+            WindowState = FormWindowState.Maximized;
             Load += Form1_Load;
             SizeChanged += Form1_Resize;
             Resize += Form1_Resize;
-            ((System.ComponentModel.ISupportInitialize)kostyButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)ruletkaButton).EndInit();
-            ((System.ComponentModel.ISupportInitialize)slotyButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gamblingButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rakhunokButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)settingsButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)exitButton).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private PictureBox kostyButton;
 
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
-        private PictureBox ruletkaButton;
-        private PictureBox slotyButton;
+        private PictureBox gamblingButton;
+        private PictureBox rakhunokButton;
+        private PictureBox settingsButton;
+        private PictureBox exitButton;
     }
 }

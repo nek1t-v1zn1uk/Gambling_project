20161d056ff854c84071f1eb506aefab30b234a0
Kaliuzhuk_Maksim <mak77chek@gmail.com>
2024-11-03 00:53:21 +0200

    Створив форму для рахунку

---
+++ RakhunokUserControl.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Gambling
+{
+    partial class RakhunokUserControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            pidtverdityButton = new PictureBox();
+            pictureTextBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pidtverdityButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureTextBox).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.Location = new Point(320, 179);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(356, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // pidtverdityButton
+            // 
+            pidtverdityButton.BackColor = Color.Transparent;
+            pidtverdityButton.Image = Properties.Resources.pidtverdity;
+            pidtverdityButton.Location = new Point(247, 424);
+            pidtverdityButton.Name = "pidtverdityButton";
+            pidtverdityButton.Size = new Size(539, 168);
+            pidtverdityButton.TabIndex = 1;
+            pidtverdityButton.TabStop = false;
+            pidtverdityButton.MouseDown += pidtverdityButton_MouseDown;
+            pidtverdityButton.MouseEnter += pidtverdityButton_MouseEnter;
+            pidtverdityButton.MouseLeave += pidtverdityButton_MouseLeave;
+            pidtverdityButton.MouseUp += pidtverdityButton_MouseUp;
+            // 
+            // pictureTextBox
+            // 
+            pictureTextBox.BackColor = SystemColors.ActiveBorder;
+            pictureTextBox.BackgroundImage = Properties.Resources.money_back;
+            pictureTextBox.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureTextBox.Location = new Point(305, 165);
+            pictureTextBox.Name = "pictureTextBox";
+            pictureTextBox.Size = new Size(388, 56);
+            pictureTextBox.TabIndex = 2;
+            pictureTextBox.TabStop = false;
+            // 
+            // RakhunokUserControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background_2;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            Controls.Add(textBox1);
+            Controls.Add(pictureTextBox);
+            Controls.Add(pidtverdityButton);
+            DoubleBuffered = true;
+            Name = "RakhunokUserControl";
+            Size = new Size(971, 592);
+            ((System.ComponentModel.ISupportInitialize)pidtverdityButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureTextBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pidtverdityButton;
+        private PictureBox pictureTextBox;
+    }
+}
---
+++ RakhunokUserControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gambling
+{
+    public partial class RakhunokUserControl : UserControl
+    {
+
+        public MainForm mainForm;
+
+        public RakhunokUserControl(Size size, MainForm form)
+        {
+            InitializeComponent();
+            mainForm = form;
+            this.Size = size;
+            Load();
+        }
+
+        public void Load()
+        {
+
+        }
+
+
+
+        private void pidtverdityButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            pidtverdityButton.Image = Properties.Resources.pidtverdity_clicked;
+        }
+
+        private void pidtverdityButton_MouseEnter(object sender, EventArgs e)
+        {
+            pidtverdityButton.Image = Properties.Resources.pidtverdity_hovered;
+        }
+
+        private void pidtverdityButton_MouseLeave(object sender, EventArgs e)
+        {
+            pidtverdityButton.Image = Properties.Resources.pidtverdity;
+        }
+
+        private void pidtverdityButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            pidtverdityButton.Image = Properties.Resources.pidtverdity;
+        }
+    }
+}
